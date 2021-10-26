4a8f2cc34eaae0aa5d63d8c7597279b13d44a7ec
dimastriann <dimastriannugraha@gmail.com>
2021-10-26 16:11:38 +0700

    [FIX] add sudo when call action window

--- asb_internal_sharing/__manifest__.py
+++ asb_internal_sharing/__manifest__.py
@@ -3,10 +3,14 @@
     'name': "ASB Sharing",
 
     'summary': """
-        Extending View""",
+        ASB Sharing Code""",
 
     'description': """
-        Inheritance / Extending View
+        => Inheritance / Extending View,
+        
+        => Dashboard Client,
+        
+        => Field Widget [ widget report / summary ]
     """,
 
     'author': "PT. Arkana Solusi Bisnis",
@@ -15,20 +19,30 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'hidden',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['web', 'sale'],
+    'depends': ['web', 'sale', 'purchase', 'mrp'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+
         'views/assets.xml',
         'views/sale_order_views.xml',
+
+        'views/dashboard_data_views.xml',
+        'views/dashboard_client_view.xml',
+
+        'views/menu_item.xml',
+
     ],
+
     # qweb
     'qweb': [
         'static/src/xml/sale_dashboard_template.xml',
+
+        'static/src/xml/dashboard_client.xml',
     ],
 }
--- asb_internal_sharing/models/__init__.py
+++ asb_internal_sharing/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import sale_orders
+from . import board_client_data
